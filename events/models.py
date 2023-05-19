efe6fdcc89e7e05ca85f88a3807c1edebfecadfc
Muhammed Afsal <muhammedafsalvr@gmail.com>
2023-05-19 21:29:28 +0530

    events models created

--- events/models.py
+++ events/models.py
@@ -1,3 +1,25 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+
+class Event(models.Model):
+    CHOICES = (
+        ('MARRIAGE' , 'marriage'),
+        ('BIRTHDAY' , 'birthday'),
+        ('FUNERAL' , 'funeral'),
+        ('ENGAREMENT','engagement'),
+        ('BAPTISM', 'baptism'),
+        ('HOUSE WARMING','house warming')
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    image = models.FileField(upload_to='events/')
+    event_type = models.CharField(max_length=255 ,choices=CHOICES)
+    event_date = models.DateField(max_length=64)
+    single_time = models.BooleanField(default=True)
+    is_deleted=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
